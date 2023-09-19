278c9bf6fbb31d2d470260bc024b171d8570a090
Ravindu Imesh Herath <94863752+Rodda2001@users.noreply.github.com>
2023-09-19 23:51:46 +0530

    first commit

---
+++ src/main/java/com/rkapp/RkappApplication.java
@@ -0,0 +1,13 @@
+package com.rkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkapp/RkappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
